5d3ba47786a46f748656995cf2fcea2d6d09cf20
Alana Muller <amuller@email.arizona.edu>
2024-10-24 18:52:03 -0700

    second guessing baseline one - can't transform negative values

--- Scripts/cortisol_analyses.R
+++ Scripts/cortisol_analyses.R
@@ -19,13 +19,12 @@ samples12Data <- readxl::read_excel("saliva_data_bySubject.xlsx", sheet = "12sam
 
 all_data <- rbind(samples9Data, samples12Data)
 
-all_data$log_cort <- log(all_data$mean_cort) # calculate log cortisol
-all_data$cort_nmol_L <- all_data$mean_cort *276 # calculate mnol/L for cortisol
-
+all_data$cort_nmol_L <- all_data$mean_cort *276 # calculate nmol/L for cortisol
+all_data$log_cort <- log(all_data$cort_nmol_L) # calculate log cortisol using nmol/L
 
 # Make dataframe to add a column to correct for the baseline for that condition
 # Make a wide version of the data
-wide_data <- pivot_wider(data = all_data, id_cols = num_cond, names_from = time, values_from = mean_cort)
+wide_data <- pivot_wider(data = all_data, id_cols = num_cond, names_from = time, values_from = cort_nmol_L)
 
 # Add baseline corrected columns
 wide_data$bc_post1 <- wide_data$post1 - wide_data$pre
@@ -59,14 +58,14 @@ ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort))
   geom_jitter() +
   stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
   facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (µg/dL)" )
+  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 # Line plot with Ss's amounts connected
 ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort)) +
   geom_point() +
   geom_line(aes(group = subjNum)) +
   facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (µg/dL)" )
+  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 # Plot with baseline corrected cort
 ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
@@ -74,14 +73,14 @@ ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort))
   geom_jitter() +
   stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
   facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (µg/dL)" )
+  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 # Line plot with Ss's amounts connected
 ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
   geom_point() +
   geom_line(aes(group = subjNum)) +
   facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (µg/dL)" )
+  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 ##### Pics without 10, 15, 20, 23, 28 that didn't finish the cp task
 
@@ -95,11 +94,12 @@ whole_cort_cpSuccess$log_cort <- log(whole_cort_cpSuccess$mean_cort)
 
 bc_cort_cpSuccess <- bc_cort %>%
   filter(!subjNum %in% participants_to_exclude)
-bc_cort_cpSuccess$log_cort <- log(bc_cort_cpSuccess$mean_cort + 1) # plus 1 so NaNs aren't produced
+bc_cort_cpSuccess$log_cort <- log(bc_cort_cpSuccess$mean_cort)
+
 
 # Make a separate dataframe with only complete cases
 # Participants to exclude
-incompletes_to_exclude <- c(1, 2, 3, 5, 6, 8, 9, 16, 22, 30)
+incompletes_to_exclude <- c(1, 2, 3, 5, 6, 8, 9, 16, 22)
 
 # Exclude data from the specified participants
 whole_cort_complete <- whole_cort_cpSuccess %>%
@@ -108,6 +108,7 @@ whole_cort_complete <- whole_cort_cpSuccess %>%
 # Exclude data from the specified participants
 bc_cort_complete <- bc_cort_cpSuccess %>%
   filter(!subjNum %in% incompletes_to_exclude)
+bc_cort_complete$log_cort <- log(bc_cort_complete$mean_cort)
 
 
 # Plot actual cortisol amount with all participant separated by condition and time
@@ -159,7 +160,7 @@ normality_allData
 
 # anova
 res.aov <- anova_test(data = whole_cort_cpSuccess, dv = log_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov)
+get_anova_table(res.aov) # condition*time interaction sig
 
 # testing simple main effects
 
@@ -169,7 +170,7 @@ one.way <- whole_cort_cpSuccess %>%
   anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
-one.way
+one.way # post15 is sig
 
 # equal cases so pairing it will work
 pwc <- whole_cort_complete %>%
@@ -178,7 +179,7 @@ pwc <- whole_cort_complete %>%
     log_cort ~ condition, paired =  TRUE,
     p.adjust.method = "bonferroni"
   )
-pwc
+pwc # cp and fire are sig diff at post15 and post30, nothing else
 
 
 one.way2 <- whole_cort_cpSuccess %>%
@@ -186,7 +187,7 @@ one.way2 <- whole_cort_cpSuccess %>%
   anova_test(dv = log_cort, wid = subjNum, within = time) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
-one.way2
+one.way2 # cp is sig
 
 # equal cases so pairing it will work
 pwc2 <- whole_cort_complete %>%
@@ -195,7 +196,7 @@ pwc2 <- whole_cort_complete %>%
     log_cort ~ time, paired = TRUE,
     p.adjust.method = "bonferroni"
   )
-pwc2
+pwc2 # cp (post15 vs post 30) and (post15 vs pre) are sig
 
 
 # Make a plot
@@ -236,11 +237,12 @@ cort_graph1
 #dev.off()
 
 ########## Stats with baseline corrected cortisol ##########
+# This one doesn't make a lot of sense since the data are still skewed but I can't transform it
 
 # Checking normality
 ggqqplot(bc_cort_complete$mean_cort)
 hist(bc_cort_complete$mean_cort)
-hist(bc_cort_complete$log_cort)
+hist(log(bc_cort_complete$mean_cort))
 ggqqplot(bc_cort_complete$log_cort)
 
 normality_bc_cort <- bc_cort_complete %>%
@@ -249,15 +251,15 @@ normality_bc_cort <- bc_cort_complete %>%
 normality_bc_cort
 
 # anova
-res.aov <- anova_test(data = bc_cort_complete, dv = log_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov) # sig condition and condition:time interaction
+res.aov <- anova_test(data = bc_cort_complete, dv = mean_cort, wid = subjNum, within = c(condition,time))
+get_anova_table(res.aov) # nothing sig
 
 # testing simple main effects
 
 # testing effect of condition at every time point
 one.way <- bc_cort_complete %>%
   group_by(time) %>%
-  anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
+  anova_test(dv = mean_cort, wid = subjNum, within = condition) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
 one.way
@@ -266,7 +268,7 @@ one.way
 pwc <- bc_cort_complete %>%
   group_by(time) %>%
   pairwise_t_test(
-    log_cort ~ condition, paired =  TRUE,
+    mean_cort ~ condition, paired =  TRUE,
     p.adjust.method = "bonferroni"
   )
 pwc
@@ -274,7 +276,7 @@ pwc
 
 one.way2 <- bc_cort_complete %>%
   group_by(condition) %>%
-  anova_test(dv = log_cort, wid = subjNum, within = time) %>%
+  anova_test(dv = mean_cort, wid = subjNum, within = time) %>%
   get_anova_table() %>%
   adjust_pvalue(method = "bonferroni")
 one.way2
@@ -283,7 +285,7 @@ one.way2
 pwc2 <- bc_cort_complete %>%
   group_by(condition) %>%
   pairwise_t_test(
-    log_cort ~ time, paired = TRUE,
+    mean_cort ~ time, paired = TRUE,
     p.adjust.method = "bonferroni"
   )
 pwc2
@@ -325,8 +327,8 @@ lines(x, y, type = "b", col = "blue", pch = 16)
 
 
 ##### Actual real data: Subset raw cort data to put it in wide form
-auc_cort <- samples12Data %>%
-  select(subjNum, condition_time, mean_cort)
+auc_cort <- whole_cort_cpSuccess %>%
+  select(subjNum, condition, time, mean_cort)
   
 auc_cort_wide <- auc_cort %>%
   pivot_wider(names_from = condition_time, values_from = mean_cort)
@@ -364,7 +366,7 @@ auc_table_long <- auc_table %>%
 
 # One way ANOVA
 hist(log(auc_table_long$auc))
-auc_table_long$auc_log <- log(auc_table_long$auc+1)
+auc_table_long$auc_log <- log(auc_table_long$auc)
 
 res.aov <- aov(auc_log ~ condition, data = auc_table_long)
 summary(res.aov) # no sig diff
