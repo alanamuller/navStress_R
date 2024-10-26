05eba4f1c7b2a3c38cea53225967d2955426cbee
Alana Muller <amuller@email.arizona.edu>
2024-10-25 18:27:17 -0700

    made it more concise

--- Scripts/cortisol_analyses.R
+++ Scripts/cortisol_analyses.R
@@ -41,107 +41,17 @@ whole_cort <- long_data %>%
   filter(time == "pre" | time == "post1" | time == "post15" | time == "post30")
 whole_cort <- whole_cort %>%
   separate_wider_delim(num_cond, delim = "_", names = c("subjNum", "condition"))
+whole_cort$log_cort <- log(whole_cort$mean_cort)
 
 bc_cort <- long_data %>%
   filter(time == "bc_post1" | time == "bc_post15" | time == "bc_post30")
 bc_cort <- bc_cort %>%
   separate_wider_delim(num_cond, delim = "_", names = c("subjNum", "condition"))
+bc_cort$log_cort <- log(bc_cort$mean_cort)
 
 # Set the order of the x-axis
 level_order <- c('pre', 'post1', 'post15', 'post30')
 bc_level_order <- c('bc_post1', 'bc_post15', 'bc_post30')
-
-
-##### Pics of all data
-
-# Plot actual cortisol amount with all participant separated by condition and time
-ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
-  facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Line plot with Ss's amounts connected
-ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=mean_cort)) +
-  geom_point() +
-  geom_line(aes(group = subjNum)) +
-  facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Plot with baseline corrected cort
-ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
-  facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Line plot with Ss's amounts connected
-ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
-  geom_point() +
-  geom_line(aes(group = subjNum)) +
-  facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-##### Pics without 10, 15, 20, 23, 28 that didn't finish the cp task
-
-# Participants to exclude
-participants_to_exclude <- c(10, 15, 20, 23, 28)
-
-# Exclude data from the specified participants
-whole_cort_cpSuccess <- whole_cort %>%
-  filter(!subjNum %in% participants_to_exclude)
-whole_cort_cpSuccess$log_cort <- log(whole_cort_cpSuccess$mean_cort)
-
-bc_cort_cpSuccess <- bc_cort %>%
-  filter(!subjNum %in% participants_to_exclude)
-bc_cort_cpSuccess$log_cort <- log(bc_cort_cpSuccess$mean_cort)
-
-
-# Make a separate dataframe with only complete cases
-# Participants to exclude
-incompletes_to_exclude <- c(1, 2, 3, 5, 6, 8, 9, 16, 22)
-
-# Exclude data from the specified participants
-whole_cort_complete <- whole_cort_cpSuccess %>%
-  filter(!subjNum %in% incompletes_to_exclude)
-
-# Exclude data from the specified participants
-bc_cort_complete <- bc_cort_cpSuccess %>%
-  filter(!subjNum %in% incompletes_to_exclude)
-bc_cort_complete$log_cort <- log(bc_cort_complete$mean_cort)
-
-
-# Plot actual cortisol amount with all participant separated by condition and time
-ggplot(data = all_data, aes(x=factor(time, level = level_order), y=mean_cort)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
-  facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Line plot with Ss's amounts connected
-ggplot(data = all_data, aes(x=factor(time, level = level_order), y=mean_cort)) +
-  geom_point() +
-  geom_line(aes(group = subjNum)) +
-  facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Plot with baseline corrected cort
-ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
-  facet_wrap(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
-
-# Line plot with Ss's amounts connected
-ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort)) +
-  geom_point() +
-  geom_line(aes(group = subjNum)) +
-  facet_grid(vars(condition)) +
-  labs(x = "Time", y = "Cortisol (nmol/L)" )
 
 ########## Checking assumptions
 
@@ -150,24 +60,24 @@ ggplot(data = bc_cort, aes(x=factor(time, level = bc_level_order), y=mean_cort))
 ##### All data
 
 # Checking normality
-ggqqplot(whole_cort_cpSuccess$mean_cort)
-hist(whole_cort_cpSuccess$mean_cort)
-hist(whole_cort_cpSuccess$log_cort)
-ggqqplot(whole_cort_cpSuccess$log_cort)
+hist(whole_cort$mean_cort)
+ggqqplot(whole_cort$mean_cort)
+hist(whole_cort$log_cort)
+ggqqplot(whole_cort$log_cort)
 
 normality_allData <- all_data %>%
   group_by(time) %>%
   shapiro_test(log_cort)
-normality_allData
+normality_allData # all still sig but better
 
 # anova
-res.aov <- anova_test(data = whole_cort_cpSuccess, dv = log_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov) # condition*time interaction sig
+res.aov <- anova_test(data = whole_cort, dv = log_cort, wid = subjNum, within = c(condition,time))
+get_anova_table(res.aov) # time and condition*time interaction sig
 
 # testing simple main effects
 
 # testing effect of condition at every time point
-one.way <- whole_cort_cpSuccess %>%
+one.way <- whole_cort %>%
   group_by(time) %>%
   anova_test(dv = log_cort, wid = subjNum, within = condition) %>%
   get_anova_table() %>%
@@ -175,7 +85,7 @@ one.way <- whole_cort_cpSuccess %>%
 one.way # post15 is sig
 
 # equal cases so pairing it will work
-pwc <- whole_cort_complete %>%
+pwc <- whole_cort %>%
   group_by(time) %>%
   pairwise_t_test(
     log_cort ~ condition, paired =  TRUE,
@@ -183,8 +93,7 @@ pwc <- whole_cort_complete %>%
   )
 pwc # cp and fire are sig diff at post15 and post30, nothing else
 
-
-one.way2 <- whole_cort_cpSuccess %>%
+one.way2 <- whole_cort %>%
   group_by(condition) %>%
   anova_test(dv = log_cort, wid = subjNum, within = time) %>%
   get_anova_table() %>%
@@ -192,38 +101,26 @@ one.way2 <- whole_cort_cpSuccess %>%
 one.way2 # cp is sig
 
 # equal cases so pairing it will work
-pwc2 <- whole_cort_complete %>%
+pwc2 <- whole_cort %>%
   group_by(condition) %>%
   pairwise_t_test(
     log_cort ~ time, paired = TRUE,
     p.adjust.method = "bonferroni"
   )
-pwc2 # cp (post15 vs post 30) and (post15 vs pre) are sig
-
-
-# Make a plot
-cond.labs <- c("Cold Pressor", "Control Condition", "Fire Environment")
-names(cond.labs) <- c("cp", "ctrl", "fire")
-
-# Plot with all participant separated by condition and time
-ggplot(data = all_data, aes(x=factor(time, level = level_order), y=log_cort)) +
-  geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
-  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
-  facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
-  labs(x = "Time", y = " Log Cortisol" )
+pwc2 # cp, (post1 vs post15), (post15 vs post 30), (post15 vs pre), (post30 vs pre) are sig
 
 # Make a plot
 cond.labs <- c("Cold Pressor", "Control", "Fire Environment")
 names(cond.labs) <- c("cp", "ctrl", "fire")
 
 # Plot with all participant separated by condition and time
-cort_graph1 <- ggplot(data = whole_cort_complete, aes(x=factor(time, level = level_order), y=log_cort)) +
+cort_graph1 <- ggplot(data = whole_cort, aes(x=factor(time, level = level_order), y=log_cort)) +
   geom_boxplot(outliers = FALSE) +
-  geom_jitter() +
+  geom_point(color = "gray60") +
   stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
+  geom_line(aes(group = subjNum), color = "gray80", alpha = 0.7) +
   facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
-  labs(x = "Time", y = " Log Cortisol" ) +
+  labs(x = "Time", y = " Log Cortisol (nmol/L)" ) +
   theme(axis.text.x = element_text(size = 13), 
         axis.text.y = element_text(size = 17), 
         axis.title.x = element_text(size = 17),
@@ -233,102 +130,40 @@ cort_graph1 <- ggplot(data = whole_cort_complete, aes(x=factor(time, level = lev
         strip.text = element_text(size = 13)) +
   scale_x_discrete(labels = c("Pre", "Post1", "Post15", "Post30"))
 
-
-#jpeg("C:/Users/amuller/Desktwhole_cort_complete#jpeg("C:/Users/amuller/Desktop/Alana/UA/HSCL/Conferences/iNAV 2024/log_cortisol1.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
+#jpeg("E:/Nav Stress Data/dissertation_pics/log_cortisol.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
 cort_graph1
 #dev.off()
 
-########## Stats with baseline corrected cortisol ##########
-# This one doesn't make a lot of sense since the data are still skewed but I can't transform it
+##### Take out the people that didn't finish the cold pressor task - SAME RESULTS AS IF THEY WERE LEFT IN THE SAMPLE
 
-# Checking normality
-ggqqplot(bc_cort_complete$mean_cort)
-hist(bc_cort_complete$mean_cort)
-hist(log(bc_cort_complete$mean_cort))
-ggqqplot(bc_cort_complete$log_cort)
+# Participants to exclude
+bad_cp <- c(10, 15, 18, 20, 23, 28)
 
-normality_bc_cort <- bc_cort_complete %>%
-  group_by(time) %>%
-  shapiro_test(mean_cort)
-normality_bc_cort
+good_cp_data <- all_data %>%
+  filter(!subjNum %in% bad_cp)
 
-# anova
-res.aov <- anova_test(data = bc_cort_complete, dv = mean_cort, wid = subjNum, within = c(condition,time))
-get_anova_table(res.aov) # nothing sig
+# Plot and ANOVA
 
-# testing simple main effects
-
-# testing effect of condition at every time point
-one.way <- bc_cort_complete %>%
-  group_by(time) %>%
-  anova_test(dv = mean_cort, wid = subjNum, within = condition) %>%
-  get_anova_table() %>%
-  adjust_pvalue(method = "bonferroni")
-one.way
-
-# equal cases so pairing it will work
-pwc <- bc_cort_complete %>%
-  group_by(time) %>%
-  pairwise_t_test(
-    mean_cort ~ condition, paired =  TRUE,
-    p.adjust.method = "bonferroni"
+summary_table2 <- good_cp_data %>%
+  group_by(condition, time, gender) %>%
+  summarize(
+    count = n(),
+    mean_cort_log = mean(log_cort), 
+    sd_cort_log = sd(log_cort)
   )
-pwc
 
+ggplot(data = summary_table2, aes(x=factor(time, level = level_order), y = mean_cort_log, color = condition, group = condition)) +
+  geom_line(size = 1.2) +
+  geom_point(size = 3) +
+  facet_wrap(~gender)
 
-one.way2 <- bc_cort_complete %>%
-  group_by(condition) %>%
-  anova_test(dv = mean_cort, wid = subjNum, within = time) %>%
-  get_anova_table() %>%
-  adjust_pvalue(method = "bonferroni")
-one.way2
-
-# equal cases so pairing it will work
-pwc2 <- bc_cort_complete %>%
-  group_by(condition) %>%
-  pairwise_t_test(
-    mean_cort ~ time, paired = TRUE,
-    p.adjust.method = "bonferroni"
-  )
-pwc2
+res.aov <- anova_test(data = good_cp_data, dv = log_cort, wid = subjNum, within = c(condition,time), between = gender)
+get_anova_table(res.aov) # time and condition:time sig
 
 
 ########### AUC stuff
 
-# Little bit of code to verify the method
-
-# calculate area under curve
-x <- c(1,2,3)
-y <- c(2,5,3)
-plot(x,y)
-lines(x,y)
-
-auc <- trapz(x,y)
-
-# Example data: 4 pairs of x and y values
-x <- c(1, 2, 3, 4)
-y <- c(2,2,2,2)
-
-# Calculate area under the curve
-auc <- trapz(x, y)
-print(paste("AUC:", auc))
-
-# Plot the curve
-plot(x, y, type = "b", col = "blue", pch = 16, xlab = "X", ylab = "Y", 
-     main = "Trapezoidal Approximation of Area Under the Curve")
-
-# Add trapezoids
-for (i in 1:(length(x) - 1)) {
-  # Draw the trapezoid by connecting consecutive points and the x-axis
-  polygon(c(x[i], x[i], x[i + 1], x[i + 1]), c(0, y[i], y[i + 1], 0), 
-          col = rgb(0.2, 0.7, 0.2, 0.4), border = "darkgreen")
-}
-
-# Re-draw the curve on top of the trapezoids
-lines(x, y, type = "b", col = "blue", pch = 16)
-
-
-##### Actual real data: Subset raw cort data to put it in wide form
+##### Subset raw cort data to put it in wide form
 auc_cort <- whole_cort_cpSuccess %>%
   select(subjNum, condition, time, mean_cort)
 auc_cort$condition_time <- paste(auc_cort$condition, auc_cort$time, sep = "_")
@@ -475,19 +310,64 @@ summary_table <- all_data %>%
     sd_cort_log = sd(log_cort)
   )
 
+# Make a plot
+
+gender_labels <- c("f" = "Women", "m" = "Men")
+condition_labels <- c("cp" = "Cold Pressor", "ctrl" = "Control", "fire" = "Fire Environment")
+
 ggplot(data = summary_table, aes(x=factor(time, level = level_order), y = mean_cort_log, color = condition, group = condition)) +
   geom_line(size = 1.2) +
   geom_point(size = 3) +
-  facet_wrap(~gender)
+  facet_wrap(~gender, labeller = labeller(gender = gender_labels)) +
+  labs(x = "Time", y = " Log Cortisol (nmol/L)", color = "condition") +
+  theme(axis.text.x = element_text(size = 13), 
+        axis.text.y = element_text(size = 17), 
+        axis.title.x = element_text(size = 17),
+        axis.title.y = element_text(size = 17),
+        legend.text = element_text(size = 17),
+        legend.title = element_text(size = 17), 
+        strip.text = element_text(size = 13)) +
+  scale_x_discrete(labels = c("Pre", "Post1", "Post15", "Post30")) +
+  scale_color_manual(labels = condition_labels, values = c("cp" = "deep sky blue", "ctrl" = "lime green", "fire" = "salmon"))
+  
 
 res.aov <- anova_test(data = all_data, dv = log_cort, wid = subjNum, within = c(condition,time), between = gender)
-get_anova_table(res.aov) # 
+get_anova_table(res.aov) # time and condition:time sig
+
+cort_graph2 <- ggplot(data = summary_table, aes(x=factor(time, level = level_order), y=log_cort)) +
+  geom_boxplot(outliers = FALSE) +
+  geom_point(color = "gray60") +
+  stat_summary(fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
+  geom_line(aes(group = subjNum), color = "gray80", alpha = 0.7) +
+  facet_wrap(vars(condition), labeller = labeller(condition = cond.labs)) +
+  labs(x = "Time", y = " Log Cortisol (nmol/L)" )
+
+#jpeg("E:/Nav Stress Data/dissertation_pics/log_cortisol.jpeg", width = 9, height = 5.75, units = 'in', res = 500)
+cort_graph2
+#dev.off()
 
 
 
 
+# graph for iNAV
+wrap_labels <- c("Inner More Familiar", "Outer More Familiar")
+names(wrap_labels) <- c("inner", "outer")
+tick_labels <- c("Novel Grids", "Outer Grids", "Inner Grids")
 
-
-
-
-
+navTrialsMean <- ggplot(NO_nav_summary, aes(x = grid_type, y = mean_grid_number, fill = condition)) + 
+  geom_boxplot(outliers = FALSE) + geom_jitter(position = position_jitterdodge()) +
+  stat_summary(aes(group = condition), fun = mean, geom = "point", shape = 18, size = 3, color = "red", position = position_dodge(0.75)) +
+  labs(x = "Grid Type", y = "Mean Grid Number", fill = "Condition") +
+  scale_x_discrete(labels = tick_labels) + 
+  scale_fill_discrete(name = "Condition", labels = c("Cold Pressor", "Control", "Fire Environment"), type = c("deep sky blue", "lime green", "salmon")) +
+  facet_wrap(vars(moreFamiliarPath), labeller = labeller(moreFamiliarPath = wrap_labels)) +
+  theme(axis.text.x = element_text(size = 13), 
+        axis.text.y = element_text(size = 17), 
+        axis.title.x = element_text(size = 17),
+        axis.title.y = element_text(size = 17),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 13), 
+        strip.text = element_text(size = 13))
+#jpeg("C:/Users/amuller/Desktop/Alana/UA/HSCL/Conferences/iNAV 2024/pics/navTrialsMedian.jpeg", width = 10, height = 5.75, units = 'in', res = 500)
+navTrialsMean
+#dev.off()
